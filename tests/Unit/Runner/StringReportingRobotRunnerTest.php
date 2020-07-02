07dd27d69726a8994ce2a1c24cbe1ed0bf313065
Edward Murrell <edward@murrell.co.nz>
2020-07-02 16:26:43 +1000

    Add additional test for command interpreter to return report strings.

--- tests/Unit/Runner/StringReportingRobotRunnerTest.php
+++ tests/Unit/Runner/StringReportingRobotRunnerTest.php
@@ -5,6 +5,9 @@ namespace Robot\Tests\Runner;
 
 use Robot\Component\Board;
 use Robot\Component\Robot;
+use Robot\Enum\Direction;
+use Robot\Instruct\Place;
+use Robot\Instruct\Report;
 use Robot\Tests\Stubs\Interpreter\InterpreterStub;
 use Robot\Tests\TestCases\SimpleTestCase;
 use Robot\Runner\StringReportingRobotRunner;
@@ -21,6 +24,25 @@ class StringReportingRobotRunnerTest extends SimpleTestCase
     {
         $expected = [];
         $interpreter = new InterpreterStub([]);
+        $runner = new StringReportingRobotRunner(new Robot(new Board(5, 5)));
+
+        $actual = \iterator_to_array($runner->runInstructions($interpreter));
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * Expect nothing from invalid reports, and multiple reports if valid.
+     */
+    public function testRunnerReportTwice(): void
+    {
+        $expected = ['1,2,NORTH', '4,3,SOUTH'];
+        $interpreter = new InterpreterStub([
+            new Report(),
+            new Place(1,2, Direction::NORTH()), new Report(),
+            new Place(4,3, Direction::SOUTH()), new Report(),
+            new Place(3,1, Direction::SOUTH())
+        ]);
         $runner = new StringReportingRobotRunner(new Robot(new Board(5, 5)));
 
         $actual = \iterator_to_array($runner->runInstructions($interpreter));
