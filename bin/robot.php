061ffeb4dfbbbba1e35876755f7c57ada933369d
Edward Murrell <edward@murrell.co.nz>
2020-07-02 16:53:00 +1000

    Implement system executable.

--- bin/robot.php
+++ bin/robot.php
@@ -2,4 +2,18 @@
 <?php
 declare(strict_types=1);
 
-echo '0,1,NORTH';
+namespace RobotRunner;
+
+use Robot\Component\Board;
+use Robot\Component\Robot;
+use Robot\Interpreter\FileInterpreter;
+use Robot\Runner\StringReportingRobotRunner;
+
+require_once dirname(__DIR__ ) . '/vendor/autoload.php';
+
+$interpreter = new FileInterpreter($argv[1]);
+$runner = new StringReportingRobotRunner(new Robot(new Board(5, 5)));
+
+foreach ($runner->runInstructions($interpreter) as $output) {
+    echo "{$output}\n";
+}
