bca03e61b651bf4f9069492efe8fb9fb21065249
Edward Murrell <edward@murrell.co.nz>
2020-07-02 16:52:41 +1000

    Fix errors in Functional tests.

--- tests/Functional/CliTest.php
+++ tests/Functional/CliTest.php
@@ -18,17 +18,17 @@ class CliTest extends CliTestCase
     public function getSuccessfulScenarios(): iterable
     {
         yield 'Example A, single response, provided in PROBLEM.md.' => [
-            'tests/Scenarios/exampleA.txt',
+            'tests/Functional/Scenarios/exampleA.txt',
             ['0,1,NORTH']
         ];
 
         yield 'Example B, single response, provided in PROBLEM.md.' => [
-            'tests/Scenarios/exampleB.txt',
+            'tests/Functional/Scenarios/exampleB.txt',
             ['0,0,WEST']
         ];
 
         yield 'Example C, single response, provided in PROBLEM.md.' => [
-            'Example C, single response, provided in PROBLEM.md.',
+            'tests/Functional/Scenarios/exampleC.txt',
             ['3,3,NORTH']
         ];
     }
@@ -43,10 +43,12 @@ class CliTest extends CliTestCase
      */
     public function testInputCapture(string $inputFile, array $expected): void
     {
-        $binary = 'php ' . dirname(__DIR__, 2) . '/bin/robot.php';
+        $binary = realpath(dirname(__DIR__, 2) . '/bin/robot.php');
+        $inputFile = realpath(dirname(__DIR__, 2) . '/' . $inputFile);
+        $command = "php {$binary} {$inputFile}";
 
         $actual = [];
-        exec("{$binary} {$inputFile}", $actual);
+        exec($command, $actual);
 
         $this->assertEquals($expected, $actual);
     }
