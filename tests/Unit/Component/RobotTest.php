185634c08d82a17262ebf0ab623d36a72781aa3e
Edward Murrell <edward@murrell.co.nz>
2020-07-02 14:21:35 +1000

    Add test for ignored commands.

--- tests/Unit/Component/RobotTest.php
+++ tests/Unit/Component/RobotTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 namespace Robot\Tests\Component;
 
 use Robot\Component\Robot;
+use Robot\Instruct\Left;
+use Robot\Instruct\Move;
+use Robot\Instruct\Right;
 use Robot\Tests\TestCases\SimpleTestCase;
 
 /**
@@ -17,6 +20,22 @@ class RobotTest extends SimpleTestCase
     public function testGettersBeforePlacement(): void
     {
         $robot = new Robot();
+
+        $this->assertNull($robot->getDirection());
+        $this->assertNull($robot->getXLoc());
+        $this->assertNull($robot->getYLoc());
+    }
+
+    /**
+     * Move, Left, Right commands do nothing when the robot has not been placed.
+     */
+    public function testRequestingMove(): void
+    {
+        $robot = new Robot();
+
+        $robot->command(new Move());
+        $robot->command(new Left());
+        $robot->command(new Right());
 
         $this->assertNull($robot->getDirection());
         $this->assertNull($robot->getXLoc());
